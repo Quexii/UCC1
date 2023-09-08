cc0f8f401ba1abf6e69d744476816e33197ac675
Quexii <adamko.mc@gmail.com>
2023-09-09 01:35:02 +0200

    Initial commit

---
+++ src/main/java/gay/shoroa/ucc/Mod.java
@@ -0,0 +1,10 @@
+package gay.shoroa.ucc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/gay/shoroa/ucc/client/Client.java
@@ -0,0 +1,10 @@
+package gay.shoroa.ucc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
